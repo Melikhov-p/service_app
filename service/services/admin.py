de85dd64c5668a11b542c9a325a3abbd44170219
Melikhov-p <melikhov.pavel@gmail.com>
2024-02-15 17:08:14 +0300

    add serializers and views for api subscriptions

--- service/services/admin.py
+++ service/services/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 
 from services.models import Service, Plan, Subscription
 
-admin.site.register(Service)
-admin.site.register(Plan)
-admin.site.register(Subscription)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('client', 'service', 'plan', 'price')
+    model = Subscription
+
+    def price(self, obj):
+        return obj.service.full_price - (obj.service.full_price * obj.plan.discount_percent / 100.00)
+
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('plan_type', 'discount_percent')
+    model = Plan
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'full_price')
+    model = Service
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
